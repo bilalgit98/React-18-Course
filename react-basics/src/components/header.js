7e235a59b927bdf366603bed444106518b3da28b
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-20 15:56:10 +0100

    Project Structure

---
+++ react-basics/src/components/header.js
@@ -0,0 +1,7 @@
+const Header = () => (
+  <div>
+    <h1> header import</h1>
+  </div>
+);
+
+export default Header;
--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./components/header";
 
 const App = () => (
-  <div>
-    <h1>hello</h1>
-  </div>
+  <>
+    <Header />
+  </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
