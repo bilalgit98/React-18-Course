507b9fd94c4615d461aab273fbdda4d133a7b0ef
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-25 17:54:42 +0100

    (BASICS) State

--- react-basics/src/components/header.js
+++ react-basics/src/components/header.js
@@ -1,26 +1,36 @@
 import React from "react";
 
-// const inputChange = () => {
-//   console.log("input changed!!");
-// };
 class Header extends React.Component {
-  inputChange = (e) => {
-    console.log(e.target.value);
+  state = {
+    title: "here is the title!",
+    keywords: "",
+    count: 0,
   };
+
+  inputChange = (e) => {
+    this.setState({
+      keywords: e.target.value,
+    });
+  };
+
+  addOne() {
+    this.setState((state, props) => ({ count: state.count + 1 }));
+  }
+
   render() {
     return (
       <>
         <div>
-          <div
-            className="logo"
-            onClick={() => {
-              console.log("lol");
-            }}
-          >
+          <div className="logo" onClick={() => console.log("lol")}>
             Logo
           </div>
           <input onChange={this.inputChange} />
         </div>
+        <br />
+        <div>{this.state.title}</div>
+        <div>{this.state.keywords}</div>
+        <div> {this.state.count}</div>
+        <button onClick={() => this.addOne()}> add one</button>
       </>
     );
   }
