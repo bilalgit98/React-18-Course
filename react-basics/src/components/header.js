dfa828ca67800e8f24779a2e9ea79f2e636a768a
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-25 17:31:06 +0100

    (BASICS) events

--- react-basics/src/components/header.js
+++ react-basics/src/components/header.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+// const inputChange = () => {
+//   console.log("input changed!!");
+// };
 class Header extends React.Component {
+  inputChange = (e) => {
+    console.log(e.target.value);
+  };
   render() {
     return (
       <>
         <div>
-          <div>Logo</div>
-          <input />
+          <div
+            className="logo"
+            onClick={() => {
+              console.log("lol");
+            }}
+          >
+            Logo
+          </div>
+          <input onChange={this.inputChange} />
         </div>
       </>
     );
