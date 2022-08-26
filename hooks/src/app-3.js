ddf17c8c6a6770b3f011e7c92fac8ce2df7920e4
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-26 16:04:27 +0100

    (HOOKS) useRef

---
+++ hooks/src/app-3.js
@@ -0,0 +1,18 @@
+import React, { useRef } from "react";
+
+const App = () => {
+  const textInput = useRef();
+  const triggerHandler = () => {
+    console.log(textInput.current.value);
+  };
+
+  return (
+    <>
+      <h1>Form:</h1>
+      <input type="text" ref={textInput} />
+      <button onClick={triggerHandler}>Trigger</button>
+    </>
+  );
+};
+
+export default App;
