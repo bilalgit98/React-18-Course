9b5e4fc6c1ff991a93f5df1f1ac792144c23c0fa
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-26 20:04:27 +0100

    (WPTBILL) Add Players to list

--- wptbill/src/components/stage_1.js
+++ wptbill/src/components/stage_1.js
@@ -1,16 +1,38 @@
 import React, { useState, useContext, useRef } from "react";
 import { Button, Form, Alert } from "react-bootstrap";
+import { MyContext } from "../context";
 
 const Stage1 = () => {
   const textInput = useRef();
+  const context = useContext(MyContext);
+  const [error, setError] = useState([false, ""]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const value = textInput.current.value;
+    const validate = validateInput(value);
 
-    console.log(value);
+    if (validate) {
+      setError([false, ""]);
+      context.addPlayer(value);
+      textInput.current.value = "";
+    }
   };
 
+  const validateInput = (value) => {
+    if (value === "") {
+      setError([true, "Please type player name"]);
+      return false;
+    }
+    if (value.length <= 2) {
+      setError([true, "You need atleast 3 characters!"]);
+      return false;
+    }
+
+    return true;
+  };
+
+  console.log(context.state);
   return (
     <>
       <Form onSubmit={handleSubmit} className="mt-4">
@@ -22,6 +44,8 @@ const Stage1 = () => {
             ref={textInput}
           />
         </Form.Group>
+
+        {error[0] ? <Alert>{error[1]}</Alert> : null}
 
         <Button className="miami" variant="primary" type="submit">
           Add Player
