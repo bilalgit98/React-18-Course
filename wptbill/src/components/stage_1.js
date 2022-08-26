9c58f15364dc0830b18c47a2b042901340a98805
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-26 19:04:53 +0100

    (WPTBILL) Creating the Form

--- wptbill/src/components/stage_1.js
+++ wptbill/src/components/stage_1.js
@@ -1,5 +1,34 @@
+import React, { useState, useContext, useRef } from "react";
+import { Button, Form, Alert } from "react-bootstrap";
+
 const Stage1 = () => {
-  return <>stage 1</>;
+  const textInput = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = textInput.current.value;
+
+    console.log(value);
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit} className="mt-4">
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Add Player Name"
+            name="player"
+            ref={textInput}
+          />
+        </Form.Group>
+
+        <Button className="miami" variant="primary" type="submit">
+          Add Player
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default Stage1;
