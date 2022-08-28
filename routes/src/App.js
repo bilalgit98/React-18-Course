791b20bf7a1d930a637f2c559fc56da3d373fad3
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-28 23:03:47 +0100

    (REACT ROUTER) Using Routes

--- routes/src/App.js
+++ routes/src/App.js
@@ -3,6 +3,14 @@ import Home from "./components/home";
 import Posts from "./components/posts";
 import Profile from "./components/profile";
 const App = () => {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="posts" element={<Posts />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
