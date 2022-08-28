7b0a4d48ce91fcd5a2034a0331a79bb154128545
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-28 22:56:29 +0100

    (REACT ROUTER)  Creating Routes

--- routes/src/App.js
+++ routes/src/App.js
@@ -1,3 +1,7 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import Profile from "./components/profile";
 const App = () => {
   return <>App</>;
 };
